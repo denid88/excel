aa2b4b45bf370ac19f343a49036136522f9c5c54
denid88 <denis.d@nekidaem.ru>
2020-06-06 15:26:14 +0300

    Finish base framework functionality

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,68 +1,10 @@
+import {createTable} from '@/components/table/table.template';
 import {ExcelComponent} from '@/core/ExcelComponent';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   toHTML() {
-    return `
-      <div class="row">
-        <div class="row-info null">
-        </div>
-        <div class="row-data">
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="row-info">1</div>
-        <div class="row-data">
-          <div class="cell" spellcheck="false" contenteditable="true">A1</div>
-          <div class="cell" spellcheck="false" contenteditable="true">B1</div>
-          <div class="cell" spellcheck="false" contenteditable="true">C1</div>
-        </div>
-      </div>`;
+    return createTable(50);
   }
 }
--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,9 +1,38 @@
+import {capitalize} from '@/core/utils';
+
 export class DOMListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('No $root provider for DomListener!');
     }
     this.$root = $root;
+    this.listeners = listeners;
+  }
+
+  initDOMListeners() {
+    console.log(this.listeners, this.$root);
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      // console.log(method);
+      // console.log(listener);
+      // Тоже самое что и addEventListener
+      if (!this[method]) {
+        const name = this.name || '';
+        throw new Error(`This method ${method} is not implemented in ${name}`);
+      }
+      this[method] = this[method].bind(this);
+      this.$root.on(listener, this[method]);
+    });
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    });
   }
 }
 
+function getMethodName(eventName) {
+  return `on${capitalize(eventName)}`;
+}
